e67c2a90a4272023e0c18c70eb82b9001d5cbf2b
ggg84 <gggggg84@outlook.it>
2021-12-20 23:11:06 +0100

    first commit

---
+++ download_google.py
@@ -0,0 +1,32 @@
+from icrawler.builtin import BaiduImageCrawler, BingImageCrawler, GoogleImageCrawler
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('query')
+parser.add_argument('--directory')
+parser.add_argument('--max', type=int, default=200)
+parser.add_argument('--prefix')
+args = parser.parse_args()
+
+if not args.directory:
+    args.directory = 'google_%s' % args.query
+
+google_crawler = GoogleImageCrawler(parser_threads=2, downloader_threads=4,
+                                    storage={'root_dir': args.directory})
+
+try:
+    google_crawler.crawl(keyword=args.query, offset=0, max_num=args.max,
+                         date_min=None, date_max=None,
+                         min_size=(200,200), max_size=None)
+finally:
+    if args.prefix is not None:
+        print "adding prefix"
+        import os
+        import glob
+        fs = glob.glob(os.path.join(args.directory, '*'))
+        for fn in fs:
+            os.rename(fn,
+                      os.path.join(os.path.split(fn)[0], args.prefix + "_" + os.path.split(fn)[1]))
+
+
---
+++ download_tumblr.py
@@ -0,0 +1,67 @@
+import urllib2
+import json
+import pandas
+from urlparse import urlparse
+import os
+from tqdm import tqdm
+import socket
+
+def create_database(tumblr, nmax=None):
+    NPOSTS = 20
+
+    tags = []
+    urls = []
+    names = []
+
+    start = 0
+
+    pbar = tqdm()
+    while True:
+        url = "https://%s.tumblr.com/api/read/json?type=photo&num=%d&start=%d#_=_" % (tumblr, NPOSTS, start)
+        pbar.write('asking new page %s' % url)
+        try:
+            response = urllib2.urlopen(url, timeout=3).read()
+        except socket.timeout:
+            pbar.write('timeout')
+            break
+        start += NPOSTS
+        response = response.replace('var tumblr_api_read = ', '').replace(';\n', '')
+        pbar.write('reading new page')
+        r = json.loads(response)
+
+        if len(r['posts']) == 0:
+            break
+
+        pbar.write('analyzing new page')
+        for ipost in range(len(r['posts'])):
+            url = r['posts'][ipost]['photo-url-400']
+            ext = os.path.splitext(url)[1]
+            if ext not in ['.jpg', '.JPG']:
+                continue
+            if 'tags' not in r['posts'][ipost].keys():
+                continue
+            tags.append(r['posts'][ipost]['tags'])
+            urls.append(url)
+            name = urlparse(r['posts'][ipost]['photo-url-400']).path.split('/')[1]
+            names.append(name)
+            pbar.update(1)
+
+        if nmax is not None and len(names) > nmax:
+            break
+
+    df = pandas.DataFrame({'uri': urls, 'tags': tags, 'image_id': names})
+    df = df.set_index('image_id')
+    df = df[~df.index.duplicated(keep='first')]
+
+    print "dowloaded: %s" % len(df)
+    
+#    df.to_csv('output_tumblr_%s.csv' % tumblr)
+    df.to_json('output_tumblr_%s.json' % tumblr)
+    
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description='Download url of tumblr images')
+    parser.add_argument('tumblr', help='the name of the tumblr')
+    parser.add_argument('--max', type=int)
+    args = parser.parse_args()
+    create_database(args.tumblr, nmax=args.max)
